022b862ae390816eb8f805387c15897b2769b9ec
vedikatenguria10-wq <vedikatenguria10@gmail.com>
2026-05-21 00:05:16 +0530

    Configured virtual environment and dependencies

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
+from pypdf import PdfReader
 
 st.set_page_config(page_title="HealthGuard AI")
 
 st.title("🩺 HealthGuard AI")
-
 st.subheader("AI-powered preventive healthcare assistant")
 
 uploaded_file = st.file_uploader(
@@ -12,4 +12,20 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file:
+
     st.success("Medical report uploaded successfully!")
+
+    pdf_reader = PdfReader(uploaded_file)
+
+    extracted_text = ""
+
+    for page in pdf_reader.pages:
+        extracted_text += page.extract_text()
+
+    st.subheader("Extracted Medical Report Text")
+
+    st.text_area(
+        "Medical Report Content",
+        extracted_text,
+        height=300
+    )
